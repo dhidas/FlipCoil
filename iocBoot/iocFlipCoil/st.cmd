cafce62c781b638c820b4b7a4e20fb0907184c38
Dean Hidas <dhidas@idioc01.nsls2.bnl.local>
2022-07-03 13:22:03 -0400

    move cmd files together

--- iocBoot/iocFlipCoil/st.cmd
+++ iocBoot/iocFlipCoil/st.cmd
@@ -7,7 +7,8 @@
 
 epicsEnvSet("ENGINEER", "Dean Andrew Hidas is not an engineer. <dhidas@bnl.gov>")
 epicsEnvSet("PMACUTIL", "/usr/share/epics-pmacutil-dev")
-epicsEnvSet("PMAC1_IP", "10.0.161.26:1025")
+#epicsEnvSet("PMAC1_IP", "192.6.94.121:1025") # Lab
+epicsEnvSet("PMAC1_IP", "10.0.161.26:1025") # Ring
 epicsEnvSet("sys", "MMF")
 epicsEnvSet("dev", "FC:2")
 epicsEnvSet("STREAM_PROTOCOL_PATH", "/usr/lib/epics/protocol:$(TOP)/proto")
@@ -19,15 +20,20 @@ epicsEnvSet("LOCATION",$(HOSTNAME))
 dbLoadDatabase("../../dbd/FlipCoil.dbd",0,0)
 FlipCoil_registerRecordDeviceDriver(pdbbase) 
 
+# Configure keithley
+#drvAsynIPPortConfigure ("KEITHLEY1", "192.6.94.120:4001")
+
+
 pmacAsynIPConfigure("P0", $(PMAC1_IP))
-pmacCreateController("PMAC1", "P0", 0, 7, 50, 500)
+pmacCreateController("PMAC1", "P0", 0, 8, 50, 500)
 pmacCreateAxis("PMAC1", 1)
 pmacCreateAxis("PMAC1", 2)
 pmacCreateAxis("PMAC1", 3)
 pmacCreateAxis("PMAC1", 4)
-pmacCreateAxis("PMAC1", 5)
-pmacCreateAxis("PMAC1", 6)
+#pmacCreateAxis("PMAC1", 5)
+#pmacCreateAxis("PMAC1", 6)
 pmacCreateAxis("PMAC1", 7)
+pmacCreateAxis("PMAC1", 8)
 
 # Create CS (ControllerPort, Addr, CSNumber, CSRef, Prog)
 # Rotary: Coordinate System 5 | PROG 5
@@ -45,8 +51,8 @@ pmacSetCoordStepsPerUnit(0, 0, 1000)
 pmacSetCoordStepsPerUnit(0, 1, 1000)
 pmacSetCoordStepsPerUnit(1, 2, 1)
 pmacSetCoordStepsPerUnit(1, 3, 1)
-pmacSetCoordStepsPerUnit(2, 4, 1)
-pmacSetCoordStepsPerUnit(2, 5, 1)
+pmacSetCoordStepsPerUnit(2, 6, 1)
+pmacSetCoordStepsPerUnit(2, 7, 1)
 
 # Set Idle and Moving poll periods (CSRef, PeriodsMilliSeconds)
 pmacSetCoordIdlePollPeriod(0, 500)
@@ -61,9 +67,10 @@ dbLoadRecords("../../db/FlipCoil.db","SYS=$(sys),DEV=$(dev),,PORT=P0")
 
 dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:X+}-Mtr,MOTOR=PMAC1,ADDR=1,DESC=X Mtr at +z, MRES=0.001, PREC=3")
 dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:X-}-Mtr,MOTOR=PMAC1,ADDR=2,DESC=X Mtr at -z, MRES=0.001, PREC=3")
-dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:R+}-Mtr,MOTOR=PMAC1,ADDR=5,DESC=Rotary Mtr at +z, MRES=0.002, PREC=3")
-dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:R-}-Mtr,MOTOR=PMAC1,ADDR=6,DESC=Rotary Mtr at -z, MRES=0.002, PREC=3")
+dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:R+}-Mtr,MOTOR=PMAC1,ADDR=7,DESC=Rotary Mtr at +z, MRES=0.002, PREC=3")
+dbLoadRecords("../../db/motor.db","P=$(sys),M={$(dev)-Ax:R-}-Mtr,MOTOR=PMAC1,ADDR=8,DESC=Rotary Mtr at -z, MRES=0.002, PREC=3")
 dbLoadRecords("../../db/asynRecord.db","P=$(sys),R={$(dev)}Asyn,ADDR=1,PORT=P0,IMAX=128,OMAX=128")
+#dbLoadRecords("../../db/Keithley2182A.db","SYS=$(sys),DEV=$(dev),PORT=KEITHLEY1")
 
 cd ../../
 set_savefile_path("as","/save")
